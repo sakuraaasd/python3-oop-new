cb27251e75bb147386da6aeb6d38a1c1b36b944c
sakuraaasd <zhangke970424@gmail.com>
2024-10-09 07:46:22 +0200

    second_commit

--- study_python/study_python.py
+++ study_python/study_python.py
@@ -1,3 +1,4 @@
+
 a = [1,2,3]
 b = [1,'abc',2.0,['a','b','c']]
 print(a)
@@ -12,4 +13,27 @@ print(c,type(c))
 s = 'abcde'
 print(s[1:3],s[-1])
 
+a = {
+  "a" : 1,
+  'c' : 2,
+  'd' : 3
+}
+print(a)
 
+s = {1,2,3,4}
+s.add(5)
+print(s)
+
+l = [1,2,3,4,5]
+l.append(6)
+print(l)
+
+a = '1234512'
+s1 = set(a)
+print(s1)
+
+s1 = {1,2,3,4}
+s2 = {3,4,5,6}
+
+print(s1 & s2)
+print(s1 | s2)
