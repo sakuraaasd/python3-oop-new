ca07a37d24f93d9c767de7bc9ca1e003949508a4
sakuraaasd <zhangke970424@gmail.com>
2024-10-09 08:02:23 +0200

    3 times change

---
+++ study_python_self/study_python_1.py
@@ -0,0 +1,24 @@
+# a = int(input('请输入一个数字: '))
+# print(a,type(a))
+
+# if a > 0:
+#   print('这是一个大于零的数字')
+
+# if 1 < 0:
+#   print('hellow world')
+#   print('1>0')
+
+# print('hellow world again')
+
+a = int(input('请输入一个数字: '))
+print(a,type(a))
+
+if a > 0:
+  print('这是一个大于零的数字')
+elif a == 0:
+  print('这是零')
+else:
+  print('这是一个小于或者等于零的数字')
+
+
+
