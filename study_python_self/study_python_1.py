50b4f477b92441309c40c60daaf545449467d084
sakuraaasd <zhangke970424@gmail.com>
2024-10-09 09:38:27 +0200

    4 times change

--- study_python_self/study_python_1.py
+++ study_python_self/study_python_1.py
@@ -10,15 +10,79 @@
 
 # print('hellow world again')
 
-a = int(input('请输入一个数字: '))
-print(a,type(a))
+#if 循环
+# a = int(input('请输入一个数字: '))
+# print(a,type(a))
 
-if a > 0:
-  print('这是一个大于零的数字')
-elif a == 0:
-  print('这是零')
+# if a > 0:
+#   print('这是一个大于零的数字')
+# elif a == 0:
+#   print('这是零')
+# else:
+#   print('这是一个小于或者等于零的数字')
+
+#while 循环
+# a = 10
+
+# while a > 0:
+#   print(a)
+#   a = a - 1
+# print('over')
+
+#for循环
+# a = '12345'
+# b = [1,2,3,4]
+# c = ('a','b','c','d')
+# d = {
+#   1:'a',
+#   2:'b',
+#   3:'c'
+# }
+
+# e = {1,2,3,5,9}
+# for item in d.items():
+#   print('{}={}'.format(a,b))
+# for item in e :
+#   print(item)
+
+# range(1,20,4)
+
+# for item in range(1,20):
+#   print(item)
+
+# for i in range(10):
+#  print(i)
+#  if i % 2 != 0:
+#   break
+
+#continue
+
+# for i in range(10):
+#   if i % 2 == 0:
+#     continue
+#   print(i)
+# import random
+
+# z = random.randint(0,100)
+# #print(type(a))
+# #b = int(a)
+# #print(type(b))
+# while True:
+#   a = int(input('请输入一个数字: '))
+#   if a == z:
+#     print('Game over.congratulations')
+#     break
+#   elif a > z :
+#     print('Big.One more time')
+#   else:
+#     print('Small.One more time')
+
+a = -1
+if a :
+  print('True')
 else:
-  print('这是一个小于或者等于零的数字')
+  print('False')
+
 
 
 
